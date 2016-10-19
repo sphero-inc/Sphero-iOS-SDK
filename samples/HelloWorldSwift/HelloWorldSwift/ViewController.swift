00782dd044e6e8086eaa4352501a8240577d836c
Corey Earwood <corey@sphero.com>
2016-10-19 00:26:35 -0600

    Update HelloWorldSwift to be a little more Swifty

--- samples/HelloWorldSwift/HelloWorldSwift/ViewController.swift
+++ samples/HelloWorldSwift/HelloWorldSwift/ViewController.swift
@@ -5,7 +5,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var robot: RKConvenienceRobot!
+    var robot: RKConvenienceRobot?
     var ledON = false
     
     @IBOutlet var connectionLabel: UILabel!
@@ -13,21 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.appWillResignActive(note:)), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.appDidBecomeActive(note:)), name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(appWillResignActive(note:)), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(appDidBecomeActive(note:)), name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
         
-        RKRobotDiscoveryAgent.shared().addNotificationObserver(self, selector: #selector(ViewController.handleRobotStateChangeNotification(notification:)))
+        RKRobotDiscoveryAgent.shared().addNotificationObserver(self, selector: #selector(handleRobotStateChangeNotification(notification:)))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
         connectionLabel = nil;
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        
-        super.init(coder: aDecoder)
     }
     
     @IBAction func sleepButtonTapped(sender: AnyObject) {
@@ -52,7 +47,6 @@ class ViewController: UIViewController {
         switch (notification.type) {
         case .connecting:
             connectionLabel.text = "\(notification.robot.name()) Connecting"
-            break
         case .online:
             let conveniencerobot = RKConvenienceRobot(robot: noteRobot);
             
@@ -64,13 +58,10 @@ class ViewController: UIViewController {
                 connectionLabel.text = noteRobot?.name()
                 togleLED()
             }
-            break
         case .disconnected:
             connectionLabel.text = "Disconnected"
             startDiscovery()
             robot = nil;
-            
-            break
         default:
             NSLog("State change with state: \(notification.type)")
         }
@@ -86,19 +77,21 @@ class ViewController: UIViewController {
     }
     
     func togleLED() {
-        if let robot = self.robot {
-            if (ledON) {
-                robot.setLEDWithRed(0.0, green: 0.0, blue: 0.0)
-            } else {
-                robot.setLEDWithRed(0.0, green: 0.0, blue: 1.0)
-            }
-            ledON = !ledON
-            
-            let delay = Int64(0.5 * Float(NSEC_PER_SEC))
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC), execute: { () -> Void in
-                self.togleLED();
-            })
+        guard let robot = self.robot else {
+            return
         }
+        
+        if (ledON) {
+            robot.setLEDWithRed(0.0, green: 0.0, blue: 0.0)
+        } else {
+            robot.setLEDWithRed(0.0, green: 0.0, blue: 1.0)
+        }
+        ledON = !ledON
+        
+        let delay = Int64(0.5 * Float(NSEC_PER_SEC))
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC), execute: { () -> Void in
+            self.togleLED();
+        })
     }
 }
 
